e8abf31d1533f19987a2eac6d39fbef96a8b5ec5
SanctusImmortalis <https://SanctusImmortalis@github.com>
2017-06-08 14:35:58 -0300

    String to IP

--- Thread.cpp
+++ Thread.cpp
@@ -6,8 +6,8 @@
 Thread::Thread(const char* ip, const char* port, Buffer* b){
   memset(&(this->addr), 0, sizeof(this->addr));
   this->addr.sin_family = AF_INET;
-this->addr.sin_addr.s_addr = INADDR_ANY;
-  //this->addr.sin_addr.s_addr = inet_addr(ip);
+  //this->addr.sin_addr.s_addr = INADDR_ANY;
+  inet_aton(ip, &(this->addr.sin_addr));
   this->addr.sin_port = htons(atoi(port));
   this->buf = b;
   this->active = true;
