8b595dc7d4348b25bdeeb8066cf1bfd3bcb1e3c0
Nelson Guedes <nelson_guedes5@hotmail.com>
2017-06-08 23:40:02 -0300

    Close socket

--- Producer.h
+++ Producer.h
@@ -1,6 +1,7 @@
 #ifndef PRODUCER_H
 #define PRODUCER_H
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,7 +14,10 @@ void* producerCode(void* arg);
 
 class Producer : public Thread{
 public:
-  Producer(bool* result, const char* ip, const char* port, Buffer* b) : Thread(result, ip, port, b) { if(*result) {bind(getSock(), (sockaddr*) &addr, sizeof(addr)); pthread_create(&t, NULL, producerCode, (void*) this);} }
+  Producer(bool* result, const char* ip, const char* port, Buffer* b) : Thread(result, ip, port, b) { if(*result) {sock = socket(AF_INET, SOCK_DGRAM, 0); bind(getSock(), (sockaddr*) &addr, sizeof(addr)); pthread_create(&t, NULL, producerCode, (void*) this);} }
+  ~Producer(){
+    close(getSock());
+  }
   void Produce();
 
   static int rear;
