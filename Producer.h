3e0804ed0811567c2c1dfb38616e6959540ff46c
Nelson Guedes <nelson_guedes5@hotmail.com>
2017-06-09 00:11:05 -0300

    Create Producer.h

--- Producer.h
+++ Producer.h
@@ -14,13 +14,23 @@ void* producerCode(void* arg);
 
 class Producer : public Thread{
 public:
-  Producer(bool* result, const char* ip, const char* port, Buffer* b) : Thread(result, ip, port, b) { if(*result) {sock = socket(AF_INET, SOCK_DGRAM, 0); bind(getSock(), (sockaddr*) &addr, sizeof(addr)); pthread_create(&t, NULL, producerCode, (void*) this);} }
+  Producer(bool* result, const char* ip, const char* port, Buffer* b) : Thread(result, ip, port, b) { 
+    if(*result) {
+      sock = socket(AF_INET, SOCK_DGRAM, 0);
+      const int val = 1;
+      const socklen_t len = sizeof(val);
+      setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void*) &val, len); 
+      bind(sock, (sockaddr*) &addr, sizeof(addr)); 
+      pthread_create(&t, NULL, producerCode, (void*) this);
+    } 
+  }
   ~Producer(){
     close(getSock());
   }
   void Produce();
 
   static int rear;
+  static int sock;
 };
 
 #endif
